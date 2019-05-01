e0a32c0252a526f5926b6f772379416f61d5c60e
Anjana Hengaju <an.hengaju@gmail.com>
2019-05-01 18:35:21 -0500

    Finsihing up

--- models/food.js
+++ models/food.js
@@ -1,27 +1,16 @@
 const orm = require("../config/orm");
 
-// orm.selectAll("foods").then(function(value) {
-//     console.log(value);
-//   });
-  
-//   orm.insertOne("foods", "food_name", "pie").then(function(value) {
-//     console.log(value);
-//   });
-
-//   orm.updateOne("foods",{id: 2, foodName: "Pizza"})
-  
 const food = {
-    all: function() {
-        return orm.selectAll("foods");
-    },
-    create: function(name) {
-        console.log(name);
-        return orm.insertOne("foods", "food_name", name);
-    },
-    update: function(id,devour){
-        return orm.updateOne("foods",`devour=${devour}`,`id=${id}`)
-    }
-}
+  all: function() {
+    return orm.selectAll("foods");
+  },
+  create: function(name) {
+    console.log(name);
+    return orm.insertOne("foods", "food_name", name);
+  },
+  update: function(id, devour) {
+    return orm.updateOne("foods", `devour=${devour}`, `id=${id}`);
+  }
+};
 
-
-module.exports = food
+module.exports = food;
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
 
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/food_controllers");
+const routes = require("./controllers/food_controllers");
 
 app.use(routes);
 
